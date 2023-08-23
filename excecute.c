61a78f51f6d45655e5833de7e0d14d281f099325
Nimoh53 <faithnimoh53@gmail.com>
2023-08-23 03:01:48 -0700

    excecute.c

---
+++ excecute.c
@@ -0,0 +1,39 @@
+#include "main.h"
+int show(char *holder, char **av, char **argv, char *path, char **env)
+{
+	pid_t child;
+	int status, count = 1;
+	path = get_Loc(argv[0]);
+	if (path == NULL)
+	{
+		err_msg(2, count, av[0], argv[0], "not found");
+		free(path);
+		free(holder);
+		exit(127);
+	}
+	child = fork();
+	if (child == -1)
+	{
+		printString("Fork Failed..\n");
+		free(holder);
+		free(path);
+		exit(127);
+	}
+	else if (child == 0)
+	{
+		if (execve(path, argv, env) == -1)
+		{
+			printf("%s: No such file or directory\n", av[0]);
+			free(holder);
+			free(path);
+			exit(127);
+		}
+	}
+	else
+	{
+		wait(&status);
+		errno = status;
+		free(path);
+		return WEXITSTATUS(status);
+	}
+}
