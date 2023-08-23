2400b38ccfeb1000420bdabc4a45ea7c1ac6fff4
johntymuriithi <muriithijohn634@gmail.com>
2023-08-23 02:52:40 -0700

    created a file

---
+++ getPath.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void find_str(char *argv, char **av)
+{
+	int count = 1;
+	if (access(argv, F_OK) != -1)
+	{
+		err_msg(2, count, av[0], argv, "not found");
+		exit(127);
+	}  
+}
